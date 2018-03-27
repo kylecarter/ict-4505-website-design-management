0f29d2700e8d9991abe5ed591735643f571ca457
kylecarter <kyle.andrew.carter@gmail.com>
2018-03-26 20:59:52 -0500

    Fixing DATABASES not defined error.
    Date:      Mon Mar 26 20:59:52 2018 -0500
    
    On branch init
    Your branch is ahead of 'origin/init' by 4 commits.
       (use "git push" to publish your local commits)
    
    Changes to be committed:
    modified:   backend/backend/settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -84,7 +84,7 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 # Database
 # https://docs.djangoproject.com/en/2.0/ref/settings/#databases
-
+DATABASES = {}
 DATABASES['default'] = dj_database_url.config( default=os.environ.get( 'DATABASE_URL' ), conn_max_age=600 )
 DATABASES['default']['ENGINE'] = 'django.db.backends.postgresql_psycopg2'
 
