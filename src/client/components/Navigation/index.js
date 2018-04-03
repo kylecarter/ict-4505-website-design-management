9bfa466637322e61ea7d4afd00e428deda44fddf
kylecarter <kyle.andrew.carter@gmail.com>
2018-04-02 22:05:17 -0500

    Adding mobile nav and other style updates.
    This address issue #9.
    
    Date:      Mon Apr 2 22:05:17 2018 -0500
    
    On branch issue/9/mobile-nav
    Changes to be committed:
    	modified:   src/client/components/Navigation/index.js
    	modified:   src/client/components/Navigation/styles.sass
    	modified:   src/client/components/Project/index.js
    	modified:   src/client/components/Project/styles.sass
    	modified:   src/client/sass/base/_icons.sass
    	modified:   src/client/sass/base/_links.sass
    	modified:   src/client/sass/base/_typography.sass
    	modified:   src/client/sass/utils/_colors.sass
    	modified:   src/client/views/Project/index.js
    	modified:   src/client/views/Project/styles.sass

--- src/client/components/Navigation/index.js
+++ src/client/components/Navigation/index.js
@@ -2,16 +2,40 @@
 
 // Node Modules
 import React from 'react'
+import underscore from 'underscore'
 
-const NavItem = ( data, index )=> {
-  return <li key={ 'react.nav.' + data.id + '.' + index }><a href={ '/project' + data.id } rel="bookmark" role="link">{ data.title }</a></li>;
+const _ = underscore;
+const window = window || {};
+
+const NavItem = ( props )=> {
+  const { activeID, data } = props;
+  return <li><a className={ activeID === data.id ? 'active' : '' } href={ '/project' + data.id } rel="bookmark" role="link">{ data.title }</a></li>;
 }
 
-export default ( props )=> {
-  return (<nav role="navigation" id="navigation" className="navigation">
-    <ul className="list-unstyled list-menu">
-      <li className="menu-home"><a href="/" rel="bookmark" role="link">Home</a></li>
-      { props.navigation.map( NavItem ) }
-    </ul>
-  </nav>);
+export default class Navigation extends React.Component {
+  constructor( props ) {
+    super( props );
+    this.state = _.extend( {
+      expose: false
+    }, props );
+    this.toggle = this.toggle.bind( this );
+  }
+
+  render() {
+    const { activeID, expose, navigation } = this.state;
+    return (<nav role="navigation" id="navigation" className={ expose ? 'navigation nav-open' : 'navigation' } onClick={ this.toggle }>
+      <ul className="list-unstyled list-menu">
+        <li className="menu-home"><a href="/" rel="bookmark" role="link">Home</a></li>
+        { navigation.map( ( data, index )=> <NavItem key={ 'react.nav.' + data.id + '.' + index } activeID={ activeID } data={ data } /> ) }
+      </ul>
+    </nav>);
+  }
+
+  toggle( e ) {
+    if ( window.innerWidth > 991 || /^a$/i.test( e.target.nodeName ) ) return this;
+    e.stopPropagation();
+    this.setState({
+      expose: !this.state.expose
+    });
+  }
 }
